251423481b236b3e49bfd4ac1a2b6fae18e97999
Sonic853 <Sonic853@qq.com>
2024-09-02 02:21:57 +0800

    set pdf

--- models/gxrb/api.ts
+++ models/gxrb/api.ts
@@ -1,7 +1,9 @@
 export const url = "https://ssw.gxrb.com.cn"
 export const urlhttp = "http://ssw.gxrb.com.cn"
+export const pdfPath = "./pdf"
 export const paperPath = "./paper"
 export const gxrbPath = `${paperPath}/gxrb`
+export const gxrbpdfath = `${pdfPath}/gxrb`
 export interface RBBODY {
   list: {
     [key: string]: RBLISTITEM
---
+++ resize_gxrb.ts
@@ -0,0 +1,39 @@
+// deno run --allow-read --allow-write --allow-env --allow-ffi resize_rmrb.ts
+import sharp from "npm:sharp@next"
+import { gxrbPath } from "./models/gxrb/api.ts"
+
+let twoPage = 0
+const papers = new Map<string, Uint8Array>()
+for await (const item of Deno.readDir(gxrbPath)) {
+  if (!item.isFile || !item.name.toLowerCase().endsWith(".jpg")) continue
+  const paper = await Deno.readFile(`${gxrbPath}/${item.name}`)
+  papers.set(item.name.split(".")[0], paper)
+}
+const sortedPapers = Array.from(papers.entries())
+  .sort(([keyA], [keyB]) => parseInt(keyA) - parseInt(keyB))
+  .map(([, value]) => value)
+for (let index = 0; index < sortedPapers.length; index++) {
+  const paper = sortedPapers[index]
+  let keyint = index + 1
+  const sharpPaper = sharp(paper)
+  const metadata = await sharpPaper.metadata()
+  const width = metadata.width
+  const height = metadata.height
+  keyint += twoPage
+  if (width && height && width > height) {
+    const paperWith = width / 2
+    const paper1 = await sharpPaper.extract({ left: 0, top: 0, width: paperWith, height }).jpeg({ quality: 100 }).toBuffer()
+    await Deno.writeFile(`${gxrbPath}/${keyint.toString().padStart(3, "0")}.jpg`, paper1)
+    twoPage++
+    keyint++
+    const sharpPaper2 = sharp(paper)
+    const paper2 = await sharpPaper2.extract({ left: paperWith, top: 0, width: paperWith, height }).jpeg({ quality: 100 }).toBuffer()
+    await Deno.writeFile(`${gxrbPath}/${keyint.toString().padStart(3, "0")}.jpg`, paper2)
+  }
+  else {
+    // const resized = await sharpPaper.resize({ height: 2048 }).jpeg({ quality: 100 }).toBuffer()
+    await Deno.writeFile(`${gxrbPath}/${keyint.toString().padStart(3, "0")}.jpg`, paper)
+  }
+}
+const textEncoder = new TextEncoder()
+await Deno.writeFile(`${gxrbPath}/size.txt`, textEncoder.encode(`${sortedPapers.length + twoPage}`))
